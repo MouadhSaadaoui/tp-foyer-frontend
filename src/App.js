97ca15f571d0b2623bb260766cbb98009d169bd8
MouadhSaadaoui <mouadh.saadaoui@esprit.tn>
2024-11-05 15:50:11 +0100

    Initial commit of frontend project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [reservations, setReservations] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8089/tpfoyer/reservation/retrieve-all-reservations')
+      .then(response => response.json())
+      .then(data => setReservations(data))
+      .catch(error => console.error('Error fetching reservations:', error));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Reservations</h1>
+        <ul>
+          {reservations.map(reservation => (
+            <li key={reservation.id}>{reservation.name}</li>
+          ))}
+        </ul>
       </header>
     </div>
   );
